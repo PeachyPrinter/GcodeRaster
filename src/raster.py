79762bc8101c6c9065660dc655cad8861450299a
James Townley <software@peachyprinter.com>
2015-06-01 15:03:54 -0600

    Got the majority of the meat done with the image processing

--- src/raster.py
+++ src/raster.py
@@ -1,12 +1,17 @@
 import logging
 import os.path
+import datetime
+import numpy as np
 from scipy import ndimage
 
 
 class Raster(object):
-    def __init__(self, laser_width=0.5, add_borders=True, output_file_name=None, overwrite=False):
+    def __init__(self, laser_width=0.5, add_borders=True, output_file_name=None):
         self.file_raster = ImageRaster(laser_width, add_borders)
-        self.output_file_name=output_file_name
+        if output_file_name:
+            self.output_file_name=output_file_name
+        else:
+            self.output_file_name='out{0}.gcode'.format(datetime.datetime.now().strftime('%Y-%b-%d-%H%M'))
 
     def process_file(self, file_name):
         with open(self.output_file_name, 'w') as output_file:
@@ -24,7 +29,72 @@ class Raster(object):
 
 class ImageRaster(object):
     def __init__(self, laser_width, add_borders):
-        pass
+        self.laser_width = laser_width
+        self.add_borders = add_borders
+        self.extrude = 0.0
+
+    def print_ascii(self, image):
+        string = '\n'
+        for y in range(0, image.shape[0]):
+            for x in range(0, image.shape[1]):
+                if np.array_equal(image[y][x], [0, 0, 0]):
+                    string += '$'
+                else:
+                    string += ' '
+            string += '\n'
+        return string + '\n-------\n'
 
     def process(self, image, height=0.0):
-        return "Some thing"
+        image = self._add_border(image)
+        self.max_y_pix = image.shape[0]
+        self.max_x_pix = image.shape[1]
+        logging.info("Image Dimensions: width: {0} height: {1}".format(self.max_x_pix, self.max_y_pix))
+
+        gcode = ""
+        for y in range(0, image.shape[0]):
+            gcode += ';row {0}\n'.format(y)
+            gcode += self._process_column(image[y], y)
+        return gcode
+
+    def _process_column(self, column, current_row):
+        state = False
+        gcode = ''
+        last_pos = 0
+        extruding_amount = 0.0
+        for column_pos in range(column.shape[0]):
+            if np.array_equal([0, 0, 0], column[column_pos]):
+                extruding_amount += 1
+                if state is False:
+                    state = True
+                    extruding_amount = 1
+                    (x, y) = self._to_real(column_pos, current_row)
+                    gcode += "G0 F0 X{:.2f} Y{:.2f} Z0.00 E{:.2f}\n".format(x, y, self.extrude)
+            else:
+                if state is True:
+                    state = False
+                    self.extrude += extruding_amount
+                    extruding_amount = 0
+                    (x, y) = self._to_real(last_pos, current_row)
+                    gcode += "G1 F1 X{:.2f} Y{:.2f} Z0.00 E{:.2f}\n".format(x, y, self.extrude)
+            last_pos = column_pos
+        if state is True:
+            self.extrude += extruding_amount
+            (x, y) = self._to_real(column_pos, current_row)
+            gcode += "G1 F1 X{:.2f} Y{:.2f} Z0.00 E{:.2f}\n".format(x, y, self.extrude)
+        return gcode
+
+
+    def _to_real(self, x, y):
+        x_pos = float(x * self.laser_width) - ((self.max_x_pix  - 1 * self.laser_width) / 2.0)
+        y_pos = float((self.max_y_pix - y) * self.laser_width) - ((self.max_y_pix + 1 * self.laser_width) / 2.0)
+        return (x_pos, y_pos)
+
+
+    def _add_border(self, image):
+        vborder = np.zeros((image.shape[0], 1, image.shape[2]))
+        toped = np.append(vborder, image, axis=1)
+        bottomed = np.append(toped, vborder, axis=1)
+        hborder = np.zeros((1, bottomed.shape[1], bottomed.shape[2]))
+        lefted = np.append(hborder, bottomed, axis=0)
+        righted = np.append(lefted, hborder, axis=0)
+        return righted
--- test/raster_test.py
+++ test/raster_test.py
@@ -5,6 +5,7 @@ import os
 import os.path
 import sys
 import scipy
+import numpy as np
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 
@@ -47,6 +48,25 @@ class RasterTest(unittest.TestCase):
             mocked_open.return_value.write.assert_called_with(output_data)
         mock_file_raster.process.assert_called_with('SomeArray')
 
+    @patch.object(os.path, 'isfile')
+    @patch.object(scipy.ndimage, 'imread')
+    def test_process_file_should_write_output_to_file_if_no_name_provided(self, mock_imread, mock_isfile, mockImageRaster):
+        output_file = 'out.gcode'
+        output_data = "some_gcode"
+        mock_isfile.return_value = True
+        mock_imread.return_value = "SomeArray"
+        mock_file_raster = mockImageRaster.return_value
+        mock_file_raster.process.return_value = output_data
+        mocked_open = mock_open()
+
+        with patch('raster.open', mocked_open, create=True):
+            rasterer = Raster()
+            rasterer.process_file("test0.png")
+            self.assertEquals('w', mocked_open.call_args[0][1])
+            self.assertTrue(mocked_open.call_args[0][0].startswith('out'))
+            mocked_open.return_value.write.assert_called(output_data)
+        mock_file_raster.process.assert_called_with('SomeArray')
+
     def test_process_file_should_not_call_file_raster_when_file_does_not_exists(self, mockImageRaster):
         mock_file_raster = mockImageRaster.return_value
         with patch('raster.open', mock_open(), create=True):
@@ -55,9 +75,69 @@ class RasterTest(unittest.TestCase):
                 rasterer.process_file("test1.png")
         self.assertEquals(0, mock_file_raster.process.call_count)
 
+
 class ImageRasterTest(unittest.TestCase):
-    def process_should_return_gcode_for_image(self):
-        pass
+    def print_ascii(self, image):
+        string = ''
+        for x in range(0, image.shape[0]):
+            for y in range(0, image.shape[1]):
+                if np.array_equal(image[x][y], [0, 0, 0]):
+                    string += '$'
+                else:
+                    string += ' '
+            string += '\n'
+        print(string + '\n-------\n')
+
+    def test_process_should_return_gcode_for_rectangle_image_with_borders(self):
+        laser_width = 1
+        image = np.array([[[0, 0, 0],[255, 255, 255]],
+                          [[0, 0, 0],[255, 255, 255]],
+                          [[255, 255, 255],[0, 0, 0]]], dtype=np.uint8)
+        # @@@@
+        # @@ @
+        # @@ @
+        # @ @@
+        # @@@@
+        expected_gcode = "".join([
+        ";row 0\n",
+        "G0 F0 X-1.50 Y2.00 Z0.00 E0.00\n",
+        "G1 F1 X1.50 Y2.00 Z0.00 E4.00\n",
+        ";row 1\n",
+        "G0 F0 X-1.50 Y1.00 Z0.00 E4.00\n",
+        "G1 F1 X-0.50 Y1.00 Z0.00 E6.00\n",
+        "G0 F0 X1.50 Y1.00 Z0.00 E6.00\n",
+        "G1 F1 X1.50 Y1.00 Z0.00 E7.00\n",
+        ";row 2\n",
+        "G0 F0 X-1.50 Y0.00 Z0.00 E7.00\n",
+        "G1 F1 X-0.50 Y0.00 Z0.00 E9.00\n",
+        "G0 F0 X1.50 Y0.00 Z0.00 E9.00\n",
+        "G1 F1 X1.50 Y0.00 Z0.00 E10.00\n",
+        ";row 3\n",
+        "G0 F0 X-1.50 Y-1.00 Z0.00 E10.00\n",
+        "G1 F1 X-1.50 Y-1.00 Z0.00 E11.00\n",
+        "G0 F0 X0.50 Y-1.00 Z0.00 E11.00\n",
+        "G1 F1 X1.50 Y-1.00 Z0.00 E13.00\n",
+        ";row 4\n",
+        "G0 F0 X-1.50 Y-2.00 Z0.00 E13.00\n",
+        "G1 F1 X1.50 Y-2.00 Z0.00 E17.00\n",])
+
+        IR = ImageRaster(laser_width, True)
+        result = IR.process(image)
+        self.assertEquals(expected_gcode, result, self.gcode_equal(expected_gcode, result))
+
+
+    def gcode_equal(self, one, two):
+        result = '\n'
+        one = one.split('\n')
+        two = two.split('\n')
+        for i in range(0, max(len(one), len(two)) - 1):
+            p1 = one[i] if i < len(one) else ''
+            p2 = two[i] if i < len(two) else ''
+            if p1 == p2:
+                result += "{:40} ==== {:40}\n".format(p1, p2)
+            else:
+                result += "{:40} !!!! {:40}\n".format(p1, p2)
+        return result
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level='DEBUG')
